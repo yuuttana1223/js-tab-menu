fea118a20050dcc2f16bbffa241c925363008d07
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-10-19 12:56:37 +0900

    [Finish] タブメニュー完成

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 "use strict";
 {
+  const menuItems = document.querySelectorAll(".menu li a");
+  const contents = document.querySelectorAll(".content");
+
+  menuItems.forEach((menuItem) => {
+    menuItem.addEventListener("click", (e) => {
+      e.preventDefault();
+      menuItems.forEach((menuItem) => {
+        menuItem.classList.remove("active");
+      });
+      menuItem.classList.add("active");
+
+      contents.forEach((content) => {
+        content.classList.remove("active");
+      });
+      document.getElementById(menuItem.dataset.id).classList.add("active");
+    });
+  });
 }
